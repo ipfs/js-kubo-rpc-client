e9b84ada884eff9b47a1e56ef2b0bc5bcfa0b5eb
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-02 18:17:37 +0000

    tmp: attempting and failing to fix...

--- test/log.spec.js
+++ test/log.spec.js
@@ -10,6 +10,9 @@ const f = factory()
 describe('.log', function () {
   this.timeout(100 * 1000)
 
+  /**
+   * @type {import('../src/index').IPFSHTTPClient}
+   */
   let ipfs
 
   before(async () => {
@@ -18,18 +21,51 @@ describe('.log', function () {
 
   after(() => f.clean())
 
-  // cannot get go-ipfs to generate logs
+  /**
+   * `ipfs log tail --help`
+   *  WARNING:   EXPERIMENTAL, command may change in future releases
+   *
+   *  USAGE
+   *    ipfs log tail - Read the event log.
+   *
+   *  SYNOPSIS
+   *    ipfs log tail
+   *
+   *  DESCRIPTION
+   *
+   *    Outputs event log messages (not other log messages) as they are generated.
+   */
   it.skip('.log.tail', async () => {
+    // const addValue = uint8ArrayFromString('just adding some data to generate logs')
+    const logLevels = ['debug', 'info', 'warn', 'error', 'dpanic', 'panic', 'fatal']
     const i = setInterval(async () => {
       try {
-        await ipfs.add(uint8ArrayFromString('just adding some data to generate logs'))
+        const randomInt = Math.floor(Math.random() * logLevels.length)
+        const randomLogLevel = logLevels[randomInt]
+        await ipfs.log.level('all', randomLogLevel)
       } catch (/** @type {any} */ _) {
         // this can error if the test has finished and we're shutting down the node
       }
     }, 1000)
 
-    const message = await first(ipfs.log.tail())
-
+    // const abortController = new AbortController()
+    // /**
+    //  * @type {import('ipfs-core-types').AbortOptions}
+    //  */
+    // const tailAbortOptions = {
+    //   signal: abortController.signal
+    // }
+    // const message = await first(ipfs.log.tail(tailAbortOptions))
+    const ipfs2 = (await f.spawn({ env: { IPFS_LOGGING: 'debug' } })).api
+    // console.log('ipfs2: ', ipfs2)
+    const res = await ipfs2.log.level('all', 'debug')
+    console.log('res: ', res)
+    expect(res).to.exist()
+    expect(res).to.be.an('object')
+    expect(res).to.not.have.property('error')
+    expect(res).to.have.property('message')
+    const message = await first(ipfs2.log.tail())
+    // setTimeout(() => abortController.abort(), 5000)
     clearInterval(i)
     expect(message).to.be.an('object')
   })
