c8b95e729519ec02bad4dcdd8914bbb49ab20b03
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-09 18:17:35 -0800

    fix: dht tests

--- test/interface-tests/src/dht/query.js
+++ test/interface-tests/src/dht/query.js
@@ -50,11 +50,11 @@ export function testQuery (factory, options) {
 
       for await (const event of nodeA.dht.query(nodeBId.id)) {
         if (event.name === 'PEER_RESPONSE') {
-          peers.push(...event.closer.map(data => data.id))
+          peers.push(...event.closer.map(data => data.id.toString()))
         }
       }
 
-      expect(peers).to.include(nodeBId.id)
+      expect(peers).to.include(nodeBId.id.toString())
     })
   })
 }
