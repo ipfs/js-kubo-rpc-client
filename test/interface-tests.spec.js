298f5343745ef5ed662f7fe40c6538a16ccd42a0
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-23 08:45:06 -0800

    fix: webworker tests pass

--- test/interface-tests.spec.js
+++ test/interface-tests.spec.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 
-import { isBrowser } from 'ipfs-utils/src/env.js'
+import { isBrowser, isWebWorker } from 'ipfs-utils/src/env.js'
 import { isWindows, isFirefox } from './constants.js'
 import * as tests from './interface-tests/src/index.js'
 import { factory } from './utils/factory.js'
@@ -89,7 +89,7 @@ function executeTests (commonFactory) {
           reason: 'https://github.com/microsoft/playwright/issues/4704#issuecomment-826782602'
         }]
       : []
-    ).concat(isBrowser
+    ).concat(isBrowser || isWebWorker
       ? [
           'should get a directory', // [mocha] output truncated to 8192 characters, see "maxDiffSize" reporter-option
           'should get a nested directory', // [mocha] output truncated to 8192 characters, see "maxDiffSize" reporter-option
