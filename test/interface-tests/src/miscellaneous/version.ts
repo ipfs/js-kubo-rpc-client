5c358a264edae42553ec6ea9a00c66845b3dc84c
Marcin Rataj <lidel@lidel.org>
2025-09-20 01:44:49 +0200

    fix: remove console.log from test to fix lint error
    removed debug console.log statement that was causing eslint no-console rule violation

--- test/interface-tests/src/miscellaneous/version.ts
+++ test/interface-tests/src/miscellaneous/version.ts
@@ -22,7 +22,6 @@ export function testVersion (factory: Factory<KuboNode>, options: MochaConfig): 
 
     it('should get the node version', async () => {
       const result = await ipfs.version()
-      console.log('IPFS version:', result.version, 'commit:', result.commit)
       expect(result).to.have.a.property('version')
       expect(result).to.have.a.property('commit')
       expect(result).to.have.a.property('repo')
